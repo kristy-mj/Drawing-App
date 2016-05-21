7cbd6ae8f3291b02978a42b7635488c95ae2d7ac
Kristy <johnstonekristy@gmail.com>
2016-05-21 16:38:41 +1200

    Create canvas and add putPoint function to draw dots on mousedown event

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 var canvas  = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
+var radius = 10;
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+var putPoint = function(e) {
+  context.beginPath();
+  context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI*2);
+  //context.arc(x, y, radius, startAngle, endAngle, [antiClockwise])
+  context.fill();
+}
+
+canvas.addEventListener('mousedown', putPoint);
